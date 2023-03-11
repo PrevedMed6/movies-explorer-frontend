1038d6525c80de329863b8f630572908ddb8d426
e.omelchenko <omel4encko.evgenya@yandex.ru>
2023-03-11 22:20:20 +0300

    feat: страница 404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import AboutProject from "../AboutProject/AboutProject";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
+import Signin from "../Login/Login";
+import Signup from "../Register/Register";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
-  return <p>Привет мир!</p>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<AboutProject />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./PageNotFound.css";
+
+function PageNotFound() {
+  return (
+    <main className="page-not-found">
+      <p className="page-not-found__error-code">404</p>
+      <p className="page-not-found__error-message">Страница не найдена</p>
+      <a className="page-not-found__nav-link" href="/">
+        Назад
+      </a>
+    </main>
+  );
+}
+
+export default PageNotFound;
