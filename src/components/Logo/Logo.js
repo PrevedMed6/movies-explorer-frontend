399ddaa5ec25d0069c9aa7965066b63a8583166f
e.omelchenko <omel4encko.evgenya@yandex.ru>
2023-05-20 22:47:25 +0300

    fix: Сайт не должен перезагружаться

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
-import logoPath from '../../images/logo.svg';
+import { NavLink } from "react-router-dom";
+import logoPath from "../../images/logo.svg";
 import "./Logo.css";
 
 function Logo() {
   return (
-  <a className="logo" href="/">
-    <img className="logo__picture" alt="Логотип" src={logoPath}/>
-  </a>
-);
+    <NavLink className="logo" to="/">
+      <img className="logo__picture" alt="Логотип" src={logoPath} />
+    </NavLink>
+  );
 }
 
 export default Logo;
